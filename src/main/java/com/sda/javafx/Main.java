d94deae7438dfea8b9f370e69bbce909febe4c4c
Adrian Giełzak <adrian.gielzak@outlook.com>
2018-02-11 14:35:28 +0100

    update widok - dwa fxml-e

--- src/main/java/com/sda/javafx/Main.java
+++ src/main/java/com/sda/javafx/Main.java
@@ -10,12 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/RootLayout.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
---
+++ src/main/java/com/sda/javafx/controller/Controller.java
@@ -0,0 +1,4 @@
+package com.sda.javafx.controller;
+
+public class Controller {
+}
